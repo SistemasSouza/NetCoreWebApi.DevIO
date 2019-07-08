74c06ee47328581130e84bc90a90fbd83e931570
Denis Souza <denyssouza1@hotmail.com>
2019-07-08 14:16:58 -0300

    Add project files.

---
+++ src/DevIO.Api/Controllers/MainController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.Api.Controllers
+{
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+         //validação de notificações de erro
+         
+        //validaçao de modelstate
+
+        //validação da operação de negocios
+    }
+}
